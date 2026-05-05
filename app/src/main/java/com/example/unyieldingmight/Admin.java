78a46b9d8ab7bd3db3c94c798cffb0b35a8dfe28
darawesome <111403788+darawesome@users.noreply.github.com>
2026-05-05 14:05:35 +1200

    Created admin class

---
+++ app/src/main/java/com/example/unyieldingmight/Admin.java
@@ -0,0 +1,30 @@
+package com.example.unyieldingmight;
+
+public class Admin {
+    private final int adminId;
+
+    Admin(Builder builder){
+        this.adminId = builder.adminId;
+    }
+
+    public int getAdminId(){
+        return adminId;
+    }
+
+    public static class Builder{
+        private int adminId;
+
+        public Builder adminId (int adminId){
+            this.adminId = adminId;
+            return this;
+        }
+    }
+
+    public manageCustomer(){
+
+    }
+
+    public manageClass(){
+
+    }
+}
