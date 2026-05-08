b608e011ac42c3900eb3a66c3f96b5f92d640dec
darawesome <111403788+darawesome@users.noreply.github.com>
2026-05-08 17:07:48 +1200

    Added create a class activity

---
+++ app/src/main/java/com/example/unyieldingmight/CreateclassActivity.java
@@ -0,0 +1,23 @@
+package com.example.unyieldingmight;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateclassActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createclass);
+
+        Spinner spinner = findViewById(R.id.activity_createclass_spinner_level);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.level_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
