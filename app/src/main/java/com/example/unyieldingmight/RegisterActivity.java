481bc6396c9ce4386ddeb0d961628e450419260c
SaplingBeing <justindegrumal28@gmail.com>
2026-05-28 10:21:30 +1200

    Added logic for register and otp

--- app/src/main/java/com/example/unyieldingmight/RegisterActivity.java
+++ app/src/main/java/com/example/unyieldingmight/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,26 +20,29 @@ public class RegisterActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void nextActivityVerify(View v){
-        Intent i = new Intent(this, VerifyActivity.class);
-        startActivity(i);
-    }
-
+// Register function
     public void register(View v){
+//        Finds the input field from activity_register.xml and assigns them to a variable
         EditText email = findViewById(R.id.activity_register_et_email);
-        String emailData = email.getText().toString();
+        String emailData = email.getText().toString().trim();
 
         EditText password = findViewById(R.id.activity_register_et_password);
-        String passwordData = password.getText().toString();
+        String passwordData = password.getText().toString().trim();
 
         EditText confirmPassword = findViewById(R.id.activity_register_et_confirmPassword);
-        String confirmPasswordData = confirmPassword.getText().toString();
+        String confirmPasswordData = confirmPassword.getText().toString().trim();
 
-        Intent i = new Intent(this, VerifyActivity.class);
-        i.putExtra(emailData,"Email");
-        i.putExtra(passwordData, "Password");
+        if(emailData.isEmpty() || passwordData.isEmpty() || confirmPasswordData.isEmpty()){
+            Toast.makeText(this, "Input fields cannot be empty", Toast.LENGTH_SHORT).show();
+        }
+        else if(!passwordData.equals(confirmPasswordData)){
+            Toast.makeText(this, "Password is not the same", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Intent i = new Intent(this, VerifyActivity.class);
+            i.putExtra(emailData,"Email");
+            i.putExtra(passwordData, "Password");
 
-        if(passwordData.equals(confirmPasswordData)){
             startActivity(i);
         }
     }
--- app/src/main/java/com/example/unyieldingmight/VerifyActivity.java
+++ app/src/main/java/com/example/unyieldingmight/VerifyActivity.java
@@ -3,6 +3,9 @@ package com.example.unyieldingmight;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,5 +19,37 @@ public class VerifyActivity extends AppCompatActivity {
     public void nextActivityMembership(View v){
         Intent i = new Intent(this, MembershipActivity.class);
         startActivity(i);
+    }
+
+    public void verify(View v){
+        EditText otp1 = findViewById(R.id.activity_verify_et_number1);
+        String otp1Data = otp1.getText().toString().trim();
+
+        EditText otp2 = findViewById(R.id.activity_verify_et_number2);
+        String otp2Data = otp2.getText().toString().trim();
+
+        EditText otp3 = findViewById(R.id.activity_verify_et_number3);
+        String otp3Data = otp3.getText().toString().trim();
+
+        EditText otp4 = findViewById(R.id.activity_verify_et_number4);
+        String otp4Data = otp4.getText().toString().trim();
+
+        EditText otp5 = findViewById(R.id.activity_verify_et_number5);
+        String otp5Data = otp5.getText().toString().trim();
+
+        EditText otp6 = findViewById(R.id.activity_verify_et_number6);
+        String otp6Data = otp6.getText().toString().trim();
+
+        if(otp1Data.isEmpty() || otp2Data.isEmpty() || otp3Data.isEmpty() || otp4Data.isEmpty() || otp5Data.isEmpty() || otp6Data.isEmpty()){
+            Toast.makeText(this, "Input fields cannot be empty", Toast.LENGTH_SHORT).show();
+        }
+        else{
+//            Otp num (String)
+            String otp = otp1Data + otp2Data + otp3Data + otp4Data + otp5Data + otp6Data;
+            Intent i = new Intent(this, MembershipActivity.class);
+            i.putExtra(otp, "Otp");
+            Log.d(otp, "Otp");
+            startActivity(i);
+        }
     }
 }
